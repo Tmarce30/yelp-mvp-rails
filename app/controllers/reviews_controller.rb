9cdc67685b7d24c340d6338a42f9ade94856f519
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-16 22:17:48 +0100

    created new review action and form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+
+  before_action :set_restaurant, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
