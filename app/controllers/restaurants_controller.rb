ca1e99768e4e92bce7c710421021c311d12c6efc
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-16 21:52:14 +0100

    added create action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,9 +13,23 @@ class RestaurantsController < ApplicationController
     @categories = ["chinese", "italian", "japanese", "french", "belgian"]
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
