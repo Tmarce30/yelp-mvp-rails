8828ab2d84fe168fc7b6b660dd4e844edeb23fbe
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-18 15:36:23 +0100

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  Faker::PhoneNumber.phone_number,
+    category:      categories.sample
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  Faker::PhoneNumber.phone_number,
+    category:      categories.sample
+  },
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  Faker::PhoneNumber.phone_number,
+    category:      categories.sample
+  },
+  {
+    name:         "La Truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  Faker::PhoneNumber.phone_number,
+    category:      categories.sample
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  Faker::PhoneNumber.phone_number,
+    category:      categories.sample
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+10.times do Restaurant.create!(
+  name:         Faker::Company.name,
+  address:      "#{Faker::Address.street_address} #{Faker::Address.zip_code}
+  #{Faker::Address.city}",
+  phone_number: Faker::PhoneNumber.phone_number,
+  category:     categories.sample
+  )
+end
+
+puts 'Finished!'
